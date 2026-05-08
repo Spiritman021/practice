2b232cec37f7ec5ebf99e3c587325ab8be253cdc
Vishal Anand <karnvishal021@gmail.com>
2026-05-08 16:53:07 +0530

    version changed

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'practice-hub-v21';
+const CACHE_NAME = 'practice-hub-v25';
 const urlsToCache = [
   './',
   './index.html',
@@ -17,7 +17,6 @@ const urlsToCache = [
   './focus-symbol-flash.html',
   './focus-stroop-tap.html',
   './focus-sequence-memory.html',
-  './focus-peripheral-tap.html',
   './power-practice.css',
   './power-practice.js',
   './mobile-overrides.css',
