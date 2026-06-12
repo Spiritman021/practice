9bcc13b54f8f52aefc275b5096c6035acc0b2000
Vishal Anand <karnvishal021@gmail.com>
2026-06-12 12:36:39 +0530

    new changes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'practice-hub-v58';
+const CACHE_NAME = 'practice-hub-v59';
 const urlsToCache = [
   './',
   './index.html',
@@ -22,6 +22,7 @@ const urlsToCache = [
   './pages/vocab/daily-class-vocab-8-june-2026.html',
   './pages/vocab/daily-class-vocab-9-june-2026.html',
   './pages/vocab/daily-class-vocab-10-june-2026.html',
+  './pages/vocab/daily-class-vocab-12-june-2026.html',
   './pages/vocab/weekly-vocab-magazine-25-30-may-2026.html',
   './pages/vocab/extra-vocab-may-month-magazine-2026.html',
 
